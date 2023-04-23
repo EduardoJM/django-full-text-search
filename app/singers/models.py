b304b3ebada35005400ac9cb840bcfd448c279c8
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-04-23 00:19:44 -0300

    refactor: move search from view to a manager (closes #1)

--- app/singers/models.py
+++ app/singers/models.py
@@ -1,9 +1,33 @@
 from django.db import models
 from django.contrib.postgres.indexes import GinIndex
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    TrigramSimilarity,
+)
+
+class SingerManager(models.Manager):
+    def search(self, term):
+        if not term:
+            return self.annotate(
+                rank=models.Value(0),
+                similarity=models.Value(0)
+            ).all()
+        vector = SearchVector("name", config="portuguese")
+        query = SearchQuery(term, config="portuguese")
+        return self.annotate(
+            search=vector,
+            rank=SearchRank(vector, query),
+            similarity=TrigramSimilarity("name", term),
+        ).filter(
+            models.Q(search=query) | models.Q(similarity__gt=0)
+        ).order_by("-rank", "-similarity").all()
 
 class Singer(models.Model):
     name = models.CharField("Cantor", max_length=150)
+
+    objects = SingerManager()
     
     def __str__(self):
         return self.name
--- app/singers/views.py
+++ app/singers/views.py
@@ -1,27 +1,9 @@
 from django.shortcuts import render
-from django.db.models import Q
-from django.contrib.postgres.search import (
-    SearchQuery,
-    SearchRank,
-    SearchVector,
-    TrigramSimilarity,
-)
 from .models import Singer
 
 def search_singer(request):
-    term = request.GET.get('q')
-    if term:
-        vector = SearchVector("name", config="portuguese")
-        query = SearchQuery(term, config="portuguese")
-        singers = Singer.objects.annotate(
-            search=vector,
-            rank=SearchRank(vector, query),
-            similarity=TrigramSimilarity("name", term),
-        ).filter(
-            Q(search=query) | Q(similarity__gt=0)
-        ).order_by("-rank", "-similarity").all()
-    else:
-        singers = Singer.objects.order_by("-id").all()
+    term = request.GET.get('q', None)
+    singers = Singer.objects.search(term)
 
     context = {
         'singers': singers,
